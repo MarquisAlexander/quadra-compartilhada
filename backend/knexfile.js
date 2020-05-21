7201e84dfa80affbafc6de4a6d40398e319ee0d1
MarquisAlexander <marquissantos123@gmail.com>
2020-05-21 11:08:19 -0300

    iniciando o backend da aplicação

---
+++ backend/knexfile.js
@@ -0,0 +1,50 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host: 'controletarefa.cb8wpsrs2beg.us-east-2.rds.amazonaws.com',
+      database: 'controletarefa',
+      user: 'marquissantos',
+      password: 'mm1234mm1234'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
